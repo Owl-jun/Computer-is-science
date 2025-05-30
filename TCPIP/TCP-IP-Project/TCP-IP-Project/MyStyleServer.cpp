9c3809e26123576cdd182c0c3c0d864c771be416
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-05-30 13:58:29 +0900

    test

--- TCPIP/TCP-IP-Project/TCP-IP-Project/MyStyleServer.cpp
+++ TCPIP/TCP-IP-Project/TCP-IP-Project/MyStyleServer.cpp
@@ -3,16 +3,17 @@
 #include <iostream>
 #include <WinSock2.h>
 #include <ws2tcpip.h>
-
+#include <hiocp.hpp>
 #pragma comment(lib, "ws2_32.lib")
 
 int main() {
 
-	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		std::cout << "WSAStartup 실패" << std::endl;
-		return 1;
-	}
+	hiocp::NetSystem netinIt;
+	//WSADATA wsaData;
+	//if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+	//	std::cout << "WSAStartup 실패" << std::endl;
+	//	return 1;
+	//}
 
 	// Socket
 	Acceptor listenSoc(12345);
